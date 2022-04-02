8417b26d3bdd4e4b185cb22c428337852cf80b67
zeearth <zeearth@gmail.com>
2022-04-02 22:25:44 +0200

    Some Work
    Add TailWind ;)

--- imports/ui/ContactForm.jsx
+++ imports/ui/ContactForm.jsx
@@ -8,7 +8,8 @@ export const ContactForm = () => {
 
   const changeValue = (event) => {
     const value = event.target.value
-    switch (event.target.name) {
+    console.log({event})
+    switch (event.target.id) {
       case 'name':
         setName(value)
         break
@@ -36,27 +37,55 @@ export const ContactForm = () => {
   }
 
   return (
-    <form>
-      <div>
-        <label htmlFor={'name'}>
-          Name
-        </label>
-        <input id={'name'} name={'name'} type={'text'} onChange={changeValue} value={name}/>
+    <form className="mt-6">
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={changeValue}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={changeValue}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700">
+            Image URL
+          </label>
+          <input
+            type="text"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={changeValue}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor={'email'}>
-          Email
-        </label>
-        <input id={'email'} name={'email'}  type={'text'} onChange={changeValue} value={email}/>
-      </div>
-      <div>
-        <label htmlFor={'imageUrl'}>
-          Image Url
-        </label>
-        <input id={'imageUrl'} name={'imageUrl'} type={'text'} onChange={changeValue} value={imageUrl}/>
-      </div>
-      <div>
-        <button type={'button'} onClick={saveContact}>Save Contact</button>
+      <div className="px-2 py-3 text-right">
+        <button
+          type="button"
+          onClick={saveContact}
+          className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
+          Save Contact
+        </button>
       </div>
     </form>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './client/**/*.{html,js}',
+    './imports/ui/**/*.{html,js,jsx}'
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
